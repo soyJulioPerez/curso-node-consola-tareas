bd8d7e15c90b76a32ae17a3ab1aa8da49d88f9e6
Julio Perez <soyJulioLPJ@bitbucket.org>
2021-04-11 10:26:43 -0300

    fix: typo

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -7,12 +7,12 @@ const preguntas = [
     message: 'Qué desea hacer?',
     choices: [
       {value: '1', name: `${'1'.cyan}. Crear tarea`},
-      {value: '2', name: `${'2'.cyan}. Listar tareas'`},
-      {value: '3', name: `${'3'.cyan}. Listar tareas completadas'`},
-      {value: '4', name: `${'4'.cyan}. Listar tareas pendientes'`},
-      {value: '5', name: `${'5'.cyan}. Completar tarea(s)'`},
-      {value: '6', name: `${'6'.cyan}. Borrar tarea'`},
-      {value: '0', name: `${'0'.cyan}. Salir'`},
+      {value: '2', name: `${'2'.cyan}. Listar tareas`},
+      {value: '3', name: `${'3'.cyan}. Listar tareas completadas`},
+      {value: '4', name: `${'4'.cyan}. Listar tareas pendientes`},
+      {value: '5', name: `${'5'.cyan}. Completar tarea(s)`},
+      {value: '6', name: `${'6'.cyan}. Borrar tarea`},
+      {value: '0', name: `${'0'.cyan}. Salir`},
     ]
   }
 ];
